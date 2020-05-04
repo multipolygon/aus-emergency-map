723b814418f3dac4fe54ed506e7fb1882efc2a3c
multipolygon <35015977+multipolygon@users.noreply.github.com>
2020-05-04 05:18:58 +0000

    Deploying to gh-pages from  @ a51aa9ed21d1387d5ebbdeb600146c84d2c57824 🚀

---
+++ user_guide/index.md
@@ -0,0 +1,41 @@
+# Australian Emergency Map
+
+## User Guide
+
+<img src="./controls.jpg" style="width: 100%;"/>
+
+## Demo Video
+
+https://youtu.be/GPqtbzj_EWg
+
+## Features
+
+<img src="./category-status-filter.jpg" style="width: 100%;"/>
+
+<img src="./warnings.jpg" style="width: 100%;"/>
+
+<img src="./max-age.jpg" style="width: 100%;"/>
+
+<img src="./sort-resources.jpg" style="width: 100%;"/>
+
+<img src="./resource-circle.jpg" style="width: 100%;"/>
+
+<img src="./side-panel.jpg" style="width: 100%;"/>
+
+<img src="./watch-zone.jpg" style="width: 100%;"/>
+
+## Notice
+
+This map is in no way officially affiliated with any emergency services, state government or data source.
+
+Do not use this app as a warning or alert service. Use it only as an analysis tool for the raw data and for seeing a big-picture overview.
+
+This is a non-commercial, volunteer project. <a href="https://github.com/reillybeacom/aus-emergency-map">Source code</a> is available and feedback or contributions welcome.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ utils.js
@@ -0,0 +1,129 @@
+// https://stackoverflow.com/a/523293
+function getSearchParam(paramName) {
+    paramName = encodeURIComponent(paramName);
+    var searchString = window.location.search.substring(1), i, val, params = searchString.split("&");
+    for (i = 0; i < params.length; i++) {
+        val = params[i].split("=");
+        if (val[0] == paramName) {
+            return decodeURIComponent(val[1]);
+        }
+    }
+    return null;
+}
+
+function objTreeSetProp(obj, prop, val) {
+    if (typeof obj === 'object') {
+        if (prop in obj) {
+            obj[prop] = val;
+        }
+        for (var k in obj) {
+            if (k != prop) {
+                objTreeSetProp(obj[k], prop, val);
+            }
+        }
+    }
+}
+
+function objTreeGetProp(obj, prop) {
+    var obj2 = {};
+    if (prop in obj) {
+        obj2[prop] = obj[prop];
+    }
+    for (var k in obj) {
+        if (typeof obj[k] === 'object') {
+            obj2[k] = objTreeGetProp(obj[k], prop);
+        }
+    }
+    return obj2;
+}
+
+function objTreeHasValue(obj, prop, val) {
+    if (typeof obj === 'object') {
+        if ((prop in obj) && obj[prop] === val) {
+            return true;
+        }
+        for (var k in obj) {
+            if (objTreeHasValue(obj[k], prop, val)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function objPack(obj, prop, val) {
+    var obj2 = {};
+    for (var k in obj) {
+        if (typeof obj[k] === 'object' && (!(prop in obj[k]) || obj[k][prop] === val)) {
+            obj2[k] = objPack(obj[k], prop, val);
+        }
+    }
+    return obj2;
+}
+
+function objMerge(obj, target, callback) {
+    if (typeof obj === 'object' && typeof target === 'object') {
+        for (var k in obj) {
+            callback(obj, target, k);
+            objMerge(obj[k], target[k], callback);
+        }
+    }
+}
+
+function parseHtmlData(html, sep) {
+    return html.split(sep || '<br />').reduce(
+        function (obj, line) {
+            var pair = line.split(':');
+            var k = pair.shift().trim().toLowerCase();
+            obj[k] = pair.join(':').trim();
+            return obj;
+        },
+        {}
+    );
+}
+
+function parseTasDescription(s) {
+    var re = '<th.*?>(.*?)<\/th><td.*?>(.*?)<\/td>';
+    return s.match(new RegExp(re, 'g')).reduce(
+        function (obj, m) {
+            var e = m.match(new RegExp(re));
+            if (e) {
+                obj[e[1]] = e[2];
+            }
+            return obj;
+        },
+        {}
+    );
+}
+
+var storageVersion = '2';
+
+function localSet(key, val) {
+    try {
+        if (localStorage && localStorage.setItem) {
+            return localStorage.setItem(key + '_v' + storageVersion, JSON.stringify(val));
+        }
+    } catch (e) { }
+    return false;
+}
+
+function localGet(key, _val) {
+    try {
+        if (localStorage && localStorage.getItem) {
+            var val = localStorage.getItem(key + '_v' + storageVersion);
+            if (val !== null) {
+                return JSON.parse(val);
+            }
+        }
+    } catch (e) { }
+    return _val;
+}
+
+function localRemove(key) {
+    try {
+        if (localStorage && localStorage.removeItem) {
+            return localStorage.removeItem(key + '_v' + storageVersion);
+        }
+    } catch (e) { }
+    return false;
+}
